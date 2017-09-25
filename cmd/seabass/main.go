e4432d948540587c44cde15ac5e7bde18090063b
Richard Rolfes <rarwolf47@github.com>
2017-09-24 21:48:21 -0400

    Stripped some code from earlier experiments

--- cmd/seabass/main.go
+++ cmd/seabass/main.go
@@ -1,46 +1,10 @@
 package main
 
 const (
-	DefaultPort = 8080
-	DefaultRoot = "/"
+	defaultPort = 8080
+	defaultRoot = "/"
 )
 
-type Application struct {
-	Title          string
-	Port           int
-	Root           string
-	MainController home.HomeController
-}
-
-func (a *Application) Init(title string) {
-	a.Title = title
-	a.Port = DefaultPort
-	a.Root = DefaultRoot
-	a.MainController.Init("Home", a.Root)
-}
-
-func (hc *HomeController) printOnPage(body string) {
-	http.HandleFunc(hc.Base.SubRoot, (func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, body)
-	}))
-}
-
-func (hc *HomeController) Index(appTitle string) {
-	pageTitle := hc.Base.Name + " - " + appTitle
-
-	hc.printOnPage("<html>" +
-		"<head>" +
-		"<title>" + pageTitle + "</title>" +
-		"</head>" +
-		"<body>" +
-		"<h1>Hello!</h1>" +
-		"</body>" +
-		"</html>")
-}
-
 func main() {
-	var app Application
-	app.Init("Project Seabass")
-	app.MainController.Index(app.Title)
-	psb.RunServer(app.Port)
+	RunServer(defaultPort)
 }
